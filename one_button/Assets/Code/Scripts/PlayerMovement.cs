2035079c2702bdf77349d93ab5d1f43618750232
Christian Rojas <113165538+Rogue-01@users.noreply.github.com>
2024-05-15 13:29:31 -0600

    Jumping to different circles

--- one_button/Assets/Code/Scripts/PlayerMovement.cs
+++ one_button/Assets/Code/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -6,13 +7,14 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private Vector3 axis = new Vector3(0, 0, 1);
     [SerializeField] private string button;
     
-    public GameObject targetToRotateAround;
+    public GameObject targetToRotateAround;//where the player pivots from 
     
     private void FixedUpdate()
     {
         if (Input.GetKey(button)) // Check if the button is held down
         {
-            RotateToTheLeft();
+            //RotateToTheLeft(); not needed anymore
+            //Jump to the next target
         }
         else
         {
@@ -28,5 +30,15 @@ public class PlayerMovement : MonoBehaviour
     private void RotateToTheRight()
     {
         transform.RotateAround(targetToRotateAround.transform.position, -axis, speed * Time.deltaTime);
+    }
+
+    private void Jump()
+    {
+        // implement the jump mechanics
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        //Find a way to change to another circle and attach itself to it use targetToRotateAround to assign the circle
     }
 }
