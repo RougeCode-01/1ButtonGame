f49502c7f37c499e37056b441b9b2b1de4b0f9a0
Christian Rojas <113165538+Rogue-01@users.noreply.github.com>
2024-05-16 18:29:46 -0600

    Implemented the jump

--- one_button/Assets/Code/Scripts/PlayerMovement.cs
+++ one_button/Assets/Code/Scripts/PlayerMovement.cs
@@ -1,20 +1,24 @@
-using System;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed = 0;
+    [SerializeField] private float jumpForce = 0;
     [SerializeField] private Vector3 axis = new Vector3(0, 0, 1);
     [SerializeField] private string button;
-    
-    public GameObject targetToRotateAround;//where the player pivots from 
-    
+    private Rigidbody2D _rigidbody2D;
+    private Transform _currentTarget; // Store the current target (collider)
+
+    private void Awake()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
     private void FixedUpdate()
     {
-        if (Input.GetKey(button)) // Check if the button is held down
+        if (Input.GetKeyDown(button))
         {
-            //RotateToTheLeft(); not needed anymore
-            //Jump to the next target
+            Jump();
         }
         else
         {
@@ -22,23 +26,46 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    private void RotateToTheLeft()
-    {
-        transform.RotateAround(targetToRotateAround.transform.position, axis, speed * Time.deltaTime);
-    }
-    
     private void RotateToTheRight()
     {
-        transform.RotateAround(targetToRotateAround.transform.position, -axis, speed * Time.deltaTime);
+        if (_currentTarget != null)
+        {
+            // Rotate around the current target
+            transform.RotateAround(_currentTarget.position, -axis, speed * Time.deltaTime);
+        }
+    }
+    private void RotateToTheLeft()
+    {
+        if (_currentTarget != null)
+        {
+            // Rotate around the current target
+            transform.RotateAround(_currentTarget.position, axis, speed * Time.deltaTime);
+        }
     }
 
     private void Jump()
     {
-        // implement the jump mechanics
+        if (_currentTarget != null)
+        {
+            // Calculate the direction from the player to the target
+            Vector2 directionToTarget = (_currentTarget.position - transform.position).normalized;
+
+            // Detach from the current target
+            _currentTarget = null;
+
+            // Apply a force in the opposite direction
+            _rigidbody2D.AddForce(-directionToTarget * jumpForce, ForceMode2D.Impulse);
+
+            Debug.Log("Jumping");
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        //Find a way to change to another circle and attach itself to it use targetToRotateAround to assign the circle
+        if (other.gameObject.CompareTag("Circle"))
+        {
+            _currentTarget = other.transform; // Set the current target
+            Debug.Log("Attached to circle");
+        }
     }
 }
